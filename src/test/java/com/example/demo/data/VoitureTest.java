411517846fb0d8b88839359999dddf83a09fa7a0
BADEDJI Antoine <badedjiantoine@gmail.com>
2026-05-19 15:49:01 +0200

    Update VoitureTest.java

--- src/test/java/com/example/demo/data/VoitureTest.java
+++ src/test/java/com/example/demo/data/VoitureTest.java
@@ -2,16 +2,28 @@ package com.example.demo.data;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.util.*;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 public class VoitureTest {
 
     @Test
-    void creerVoiture(){
-        assertEquals(1,1);
+    void testCreationEtGetters() {
+        Voiture voiture = new Voiture();
+        voiture.setId(1L);
+        voiture.setMarque("Renault");
+        voiture.setPrix(15000);
+        voiture.setEnPanne(false);
+        assertEquals(1L, voiture.getId());
+        assertEquals("Renault", voiture.getMarque());
+        assertEquals(15000, voiture.getPrix());
+        assertFalse(voiture.isEnPanne());
     }
 
+    @Test
+    void testEchouer() {
+        Voiture voiture = new Voiture();
+        voiture.setMarque("Peugeot");
+        assertEquals("Citroen", voiture.getMarque(), "La marque ne correspond pas !");
+    }
 }
